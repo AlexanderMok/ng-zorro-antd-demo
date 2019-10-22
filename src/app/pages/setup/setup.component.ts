68c22bfe0ff188b54e63640c403c97527596d2e5
moqinglai <qinglai.mo@shatacloud.com>
2019-10-22 15:35:19 +0800

    1.add html and less contents to setup module. 2.import NgZorroAntdModule and FormsModule into setup module

--- src/app/pages/setup/setup.component.ts
+++ src/app/pages/setup/setup.component.ts
@@ -1,4 +1,10 @@
+import { LocalStorageService } from './../../services/local-storage/local-storage.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  USERNAME,
+  INIT_FLAG,
+  START_USING_DATE
+} from '../../services/local-storage/local-storage.namespace';
 
 @Component({
   selector: 'app-setup',
@@ -6,10 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setup.component.less']
 })
 export class SetupComponent implements OnInit {
-
-  constructor() { }
+  username: string;
+  constructor(private store: LocalStorageService) { }
 
   ngOnInit() {
   }
 
+  public completeSetup(): void {
+    this.store.set(INIT_FLAG, true);
+    // this.store.set(START_USING_DATE, '');
+    this.store.set(USERNAME, this.username);
+  }
 }
--- src/app/pages/setup/setup.module.ts
+++ src/app/pages/setup/setup.module.ts
@@ -2,12 +2,17 @@ import { SetupRoutingModule } from './setup-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupComponent } from './setup.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   imports: [
-    CommonModule, SetupRoutingModule
+    CommonModule,
+    SetupRoutingModule,
+    NgZorroAntdModule, /* import ng-zorro-antd module so that it can be used in sub module*/
+    FormsModule /* import this module so that two-way binding of input can be used in sub module*/
   ],
   declarations: [SetupComponent],
   exports: [SetupComponent]
