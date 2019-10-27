571e545a11a39d4ae882bb704f52aff1c65ca15c
moqinglai <qinglai.mo@shatacloud.com>
2019-10-27 16:52:29 +0800

    add routing guard to app-routing.module.ts and main-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitGuardService } from './services/init-guard/init-guard.service';
 
 
 const routes: Routes = [
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: 'setup', loadChildren: () => import('./pages/setup/setup.module').then(m => m.SetupModule) }, /* lazy load*/
+  {
+    path: 'setup',
+    loadChildren: () => import('./pages/setup/setup.module').then(m => m.SetupModule),
+    canActivate: [InitGuardService]
+  }, /* lazy load*/
   { path: 'main', pathMatch: 'full', redirectTo: '/main' }, /* eager load. need to import maodule in app.module.ts */
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' }
+  { path: '', pathMatch: 'full', redirectTo: '/setup' }
 ];
 
 @NgModule({
--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -3,12 +3,14 @@ import { MainComponent } from './main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
+import { InitGuardService } from 'src/app/services/init-guard/init-guard.service';
 
 
 const routes: Routes = [
   {
     path: 'main',
     component: MainComponent,
+    canActivate: [InitGuardService],
     children: [
       {
         path: ':id',
