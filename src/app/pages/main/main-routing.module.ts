fd9482c40fffecabf620891a7ed3b3f8ccba3593
moqinglai <qinglai.mo@shatacloud.com>
2019-10-27 15:08:23 +0800

    1.add pages/main/detail component 2.add DetailComponent routing

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -1,10 +1,22 @@
+import { ListComponent } from './left-control/list/list.component';
 import { MainComponent } from './main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
 
 
 const routes: Routes = [
-  { path: 'main', component: MainComponent }
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      {
+        path: ':id',
+        component: DetailComponent,
+        pathMatch: 'full'
+      }
+    ]
+  }
 ];
 
 @NgModule({
